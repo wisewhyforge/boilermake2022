9e55724cbd44fc624b0747e24f745aa9a4beba56
Sean <35261676+SeanZ7@users.noreply.github.com>
2022-01-22 18:40:30 -0500

    attempt

--- send_message.py
+++ send_message.py
@@ -1,10 +1,9 @@
-import os
 from twilio.rest import Client
 
 
 def send_message(phone, message):
-    account_sid = os.environ['ACa4e93b4e7611ad9b0568d965d8d0057c']
-    auth_token = os.environ['749896b60dcd8d774ab33977b1db5b87']
+    account_sid = 'ACa4e93b4e7611ad9b0568d965d8d0057c'
+    auth_token = '749896b60dcd8d774ab33977b1db5b87'
 
     client = Client(account_sid, auth_token)
 
