aaf2dfd6f70d07b8ba44e7f87a82cc1119eb923a
Sean <35261676+SeanZ7@users.noreply.github.com>
2022-01-22 15:33:47 -0500

    Split adding item and deleting item into different files

--- edit_items.py
+++ edit_items.py
@@ -3,8 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import date
 
-def consumption(phone, item_to_delete):
-
+def delete_item(phone, item):
     cred = credentials.ApplicationDefault()
     firebase_admin.initialize_app(cred, {
         'projectId': 'hackathon2022'
@@ -16,44 +15,11 @@ def consumption(phone, item_to_delete):
     doc = collection.document(phone)
     dict = doc.get().to_dict()
 
-    for i in range(len(dict)):
-        if dict.keys()[i] == item_to_delete:
-            del dict[item_to_delete]
+    for i in dict:
+        if dict[i] == item:
+            del dict[item]
 
-    doc_ref = db.collection(u'users').document(phone)
-    doc_ref.set(dict)
-
-    
-    
-def add_items_to_db(phone, items):
-    cred = credentials.ApplicationDefault()
-    firebase_admin.initialize_app(cred, {
-        'projectId': 'hackathon2022'
-    })
-
-    db = firestore.client()
-
-    today = date.today().strftime("%m%d%y")
-    expiration = {}
-
-    for item in items:
-        foodexp = db.collection(u'expiration').document(item).get().to_dict()
-
-        expiration[item + '_' + today + '_' + items[item]] = foodexp[items[item]]
-
-    doc_ref = db.collection(u'users').document(phone)
-    doc_ref.set(expiration)
-
-phone = '7978421398'
-
-items = {
-    'onion': 'frozen',
-    'grape': 'frozen',
-    'apple': 'frozen'
-}
-
-add_items_to_db(phone, items)
+    doc.set(dict)
 
 
-
-
+delete_item()
--- remove_items.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-def remove_item(user, item, db):
-    user_dict = db.collection('users').document(user).get().to_dict()
-
-
