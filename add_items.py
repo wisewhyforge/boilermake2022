d22251fb3d7e9131ed9d6d3d8386d8063599d5f1
Sean <35261676+SeanZ7@users.noreply.github.com>
2022-01-22 12:50:57 -0500

    Testing adding stuff to db

---
+++ add_items.py
@@ -0,0 +1,33 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import date
+
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'projectId': 'hackathon2022'
+})
+
+db = firestore.client()
+
+today = date.today().strftime("%d%m%y")
+phone = '7978421398'
+
+items = {
+    'onion': 'frozen',
+    'grape': 'refrigerated',
+    'apple': 'room'
+}
+
+expiration = {}
+
+for item in items:
+    foodexp = db.collection(u'expiration').document(item).get().to_dict()
+
+    expiration[item + today] = foodexp[item]
+
+doc_ref = db.collection(u'users').document(phone)
+doc_ref.set(expiration)
+
+
+
